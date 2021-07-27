fb9be1f26ec7721d51edb172fc99d0bf35e4319f
roberm22 <79532837+roberm22@users.noreply.github.com>
2021-07-27 18:00:09 +0200

    Laboratorio 2

---
+++ lab2/G14-L2-mod.js
@@ -0,0 +1,35 @@
+
+ console.log("Roberto Martin");
+ console.log("Laboratorio 2 - G14: Tabla de unidades binarias");
+ console.log("Grupo 14");
+
+function unidad(x)  {
+
+     if (x===10)  {return "Kibi";}
+else if (x===20)  {return "Mebi";}
+else if (x===30)  {return "Gibi";}
+else if (x===40)  {return "Tebi";}
+else if (x===50)  {return "Pebi";}
+else if (x===60)  {return "Exbi";}
+else if (x===70)  {return "Zebi";}
+else if (x===80)  {return "Yobi";}
+else if (x===90)  {return "Bronto";}
+else if (x===100)  {return "Geop";}
+
+
+
+
+}
+
+
+ console.log("23 x 2^10 Bytes son 23" + unidad(10) + " bytes");
+ console.log("23 x 2^20 Bytes son 23" + unidad(20) + " bytes");
+ console.log("23 x 2^30 Bytes son 23" + unidad(30) + " bytes");
+ console.log("23 x 2^40 Bytes son 23" + unidad(40) + " bytes");
+ console.log("23 x 2^50 Bytes son 23" + unidad(50) + " bytes");
+ console.log("23 x 2^60 Bytes son 23" + unidad(60) + " bytes");
+ console.log("23 x 2^70 Bytes son 23" + unidad(70) + " bytes");
+ console.log("23 x 2^80 Bytes son 23" + unidad(80) + " bytes");
+ console.log("23 x 2^90 Bytes son 23" + unidad(90) + " bytes");
+ console.log("23 x 2^100 Bytes son 23" + unidad(100) + " bytes");
+
---
+++ lab2/G14-L2.js
@@ -0,0 +1,24 @@
+function unidad(x)  {
+
+     if (x==10)  {return "Kibi";}
+else if (x==20)  {return "Mebi";}
+else if (x==30)  {return "Gibi";}
+else if (x==40)  {return "Tebi";}
+else if (x==50)  {return "Pebi";}
+else if (x==60)  {return "Exbi";}
+else if (x==70)  {return "Zebi";}
+else if (x==80)  {return "Yobi";}
+
+
+}
+
+ console.log("Roberto Martin");
+ console.log("Grupo 14")
+ console.log("23 x 2^10 Bytes son 23" + unidad(10) + "bytes");
+ console.log("23 x 2^20 Bytes son 23" + unidad(20) + "bytes");
+ console.log("23 x 2^30 Bytes son 23" + unidad(30) + "bytes");
+ console.log("23 x 2^40 Bytes son 23" + unidad(40) + "bytes");
+ console.log("23 x 2^50 Bytes son 23" + unidad(50) + "bytes");
+ console.log("23 x 2^60 Bytes son 23" + unidad(60) + "bytes");
+ console.log("23 x 2^70 Bytes son 23" + unidad(70) + "bytes");
+ console.log("23 x 2^80 Bytes son 23" + unidad(80) + "bytes");
